8699d2779f36543f32a539ccb770e1c0c53261b2
wenliang <wenliang@163.com>
2024-01-04 16:21:26 +0800

    [修改] 验证文件描述符占用的是空闲的最小的一个

--- FileIO/testOpen.c
+++ FileIO/testOpen.c
@@ -8,14 +8,43 @@
 int main()
 {
     /* 打开文件 */
-    int fd = open("./hello.txt", O_RDWR | O_CREAT, 0644);
-    if (fd == -1)
+    int fd1 = open("./hello.txt", O_RDWR | O_CREAT, 0644);
+    if (fd1 == -1)
     {
         perror("open error");
     }
-    /* 关闭文件 */
-    close(fd);
+    printf("fd1 = %d\n", fd1);
 
+    int fd2 = open("./hello.txt", O_RDWR | O_CREAT, 0644);
+    if (fd2 == -1)
+    {
+        perror("open error");
+    }
+    printf("fd2 = %d\n", fd2);
+
+    int fd3 = open("./hello.txt", O_RDWR | O_CREAT, 0644);
+    if (fd3 == -1)
+    {
+        perror("open error");
+    }
+    printf("fd3 = %d\n", fd3);
+
+    int fd4 = open("./hello.txt", O_RDWR | O_CREAT, 0644);
+    if (fd4 == -1)
+    {
+        perror("open error");
+    }
+    printf("fd4 = %d\n", fd4);
+    
+    /* 关闭文件fd1. */
+    close(fd1);
+
+    int fd5 = open("./hello.txt", O_RDWR | O_CREAT, 0644);
+    if (fd5 == -1)
+    {
+        perror("open error");
+    }
+    printf("fd5 = %d\n", fd5);
 
     return 0;
 }
