50bb859e182836663a185a378737703319b85340
wenliang <wenliang@163.com>
2024-01-06 10:47:52 +0800

    [修改] 命令行参数的用法

---
+++ FileIO/testCommandParam.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main(int argc, const char *argv[])
+{
+    /* argc是参数的个数 */
+    printf("argc:%d\n", argc);
+
+    printf("argv[0]:%s\n", argv[0]);
+    printf("argv[1]:%s\n", argv[1]);
+    printf("argv[2]:%s\n", argv[2]);
+
+    return 0;
+}
